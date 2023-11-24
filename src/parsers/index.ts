2c92a44c98f91032f6ef5813938d6e1d03e42c22
Jakub Skopal <jakub.skopal@bindworks.eu>
2023-11-24 16:28:37 +0100

    add parsed data

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,5 +1,58 @@
 export interface ParsedData {
-
+    periodStart?: Date,
+    periodEnd?: Date,
+    /**
+     * percentage from 0 to 1
+     */
+    timeActive?: number,
+    /**
+     * percentage from 0 to 1
+     */
+    timeInRangeVeryHigh?: number,
+    /**
+     * percentage from 0 to 1
+     */
+    timeInRangeHigh?: number,
+    /**
+     * percentage from 0 to 1
+     */
+    timeInRangeNormal?: number,
+    /**
+     * percentage from 0 to 1
+     */
+    timeInRangeLow?: number,
+    /**
+     * percentage from 0 to 1
+     */
+    timeInRangeVeryLow?: number,
+    /**
+     * mmol/l
+     */
+    averageGlucose?: number,
+    /**
+     * mmol/l
+     */
+    stddevGlucose?: number,
+    /**
+     * mmol/mol
+     */
+    gmi?: number,
+    /**
+     * units
+     */
+    dailyInsulinDose?: number,
+    /**
+     * units
+     */
+    basalInsulin?: number,
+    /**
+     * units
+     */
+    correctionInsulin?: number,
+    /**
+     * units
+     */
+    bolusInsulin?: number,
 }
 
 export type ReportParser = (pdfPath: string) => Promise<ParsedData>;
