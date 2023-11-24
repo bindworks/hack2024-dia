660253cdec00be59e8d0599099669f07d762a678
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-11-24 16:46:12 +0100

    edtior config and tsv

--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,40 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 export interface ExecuteOutput {
-    stdout: string;
-    stderr: string;
+  stdout: string;
+  stderr: string;
 }
 
 export async function execute(command: string): Promise<ExecuteOutput> {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve({ stdout, stderr });
-            }
-        })
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ stdout, stderr });
+      }
     });
+  });
 }
 
 export interface PdfToTextSettings {
-    layout?: boolean;
+  layout?: boolean;
+  tsv?: boolean;
 }
 
 export async function pdfToText(pdfPath: string, settings?: PdfToTextSettings): Promise<string> {
-    const command = ["pdftotext"];
-    if (settings?.layout) {
-        command.push("-layout");
-    }
-    command.push(`"${pdfPath}"`);
-    command.push(`-`);
-    
-    const out = await execute(command.join(" "));
-    return out.stdout;
+  const command = ["pdftotext"];
+  if (settings?.layout) {
+    command.push("-layout");
+  }
+
+  if (settings?.tsv) {
+    command.push("-tsv");
+  }
+
+  command.push(`"${pdfPath}"`);
+  command.push(`-`);
+
+  const out = await execute(command.join(" "));
+  return out.stdout;
 }
