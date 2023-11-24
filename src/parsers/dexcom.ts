c619b1de733012e0fad319eb65f81b8501533709
Milan Savickij <milan.savitskiy@email.cz>
2023-11-24 19:11:42 +0100

    feat: parse czech dexcom date

--- src/parsers/dexcom.ts
+++ src/parsers/dexcom.ts
@@ -1,7 +1,25 @@
 import { ParsedData } from ".";
 import { pdfToText } from "../utils";
 
+const monthShortcuts = {
+  cz: new Map([
+    ["led", 0],
+    ["úno", 1],
+    ["bře", 2],
+    ["dub", 3],
+    ["kvě", 4],
+    ["čvn", 5],
+    ["čvc", 6],
+    ["srp", 7],
+    ["zář", 8],
+    ["říj", 9],
+    ["lis", 10],
+    ["pro", 11],
+  ]),
+};
+
 const dexcomRegexes = {
+  date: /\S{2}\s*(\d+).\s*(\S{3})\s*(\d{4})/gm,
   patientNameAndTimeFrame:
     /(?:Přehled|Súhrn|Overview)\s*(\d+)\s*(?:dny\/dní|dní|days)\s*\|\s*((?:\S{2}\s*\d+\.\s*\S{3}\s*\d{4})|(?:\S{3}\s*\S{3}\s*\d{1,2},\s*\d{4}))\s*-\s*((?:\S{2}\s*\d+\.\s*\S{3}\s*\d{4})|(?:\S{3}\s*\S{3}\s*\d{1,2},\s*\d{4}))\s*(\S+\s*\S+)/gm,
   timeAtCGM:
@@ -21,6 +39,31 @@ const dexcomRegexes = {
     veryLow:
       /(?:Nízká|Nízke|Low)\s*<?(\d+(?:(?:,|.)\d+)?)\s*%\s*(?:Velmi|Veľmi|Very)\s*(?:Nízký|Nízke|Low)/gm,
   },
+};
+
+const parseDate = (date: string): Date => {
+  const dateParsed = dexcomRegexes.date.exec(date);
+  const dateYear = dateParsed?.[3];
+  const dateMonth = dateParsed?.[2];
+  const dateDay = dateParsed?.[1];
+
+  if (!dateYear || !dateMonth || !dateDay) {
+    throw new Error("Could not parse Dexcom PDF Date");
+  }
+
+  const dateMonthValue = monthShortcuts.cz.get(dateMonth);
+
+  if (!dateMonthValue) {
+    throw new Error("Could not parse Dexcom PDF Date Month");
+  }
+
+  const dateDate = new Date(
+    parseInt(dateYear),
+    dateMonthValue,
+    parseInt(dateDay)
+  );
+
+  return dateDate;
 };
 
 export async function dexcomParser(pdfPath: string): Promise<ParsedData> {
@@ -64,7 +107,12 @@ export async function dexcomParser(pdfPath: string): Promise<ParsedData> {
     throw new Error("Could not parse Dexcom PDF");
   }
 
+  const periodStart = parseDate(timeFrameStart);
+  const periodEnd = parseDate(timeFrameEnd);
+
   return {
+    periodStart,
+    periodEnd,
     timeInRangeVeryHigh: parseFloat(timeInRange.veryHigh),
     timeInRangeHigh: parseFloat(timeInRange.high),
     timeInRangeNormal: parseFloat(timeInRange.moderate),
