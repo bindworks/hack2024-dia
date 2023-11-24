52d05a9ce2b6642836d3989ca74e4ba5b7976c38
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-11-24 18:00:26 +0100

    libre average glucose

--- src/classifier.ts
+++ src/classifier.ts
@@ -1,24 +1,29 @@
 import { ReportParser } from "./parsers";
 import { dexcomParser } from "./parsers/dexcom";
+import { libreAGPParser } from "./parsers/libre";
 import { medtronik640GParser } from "./parsers/medtronik640G";
 import { pdfToText } from "./utils";
 
 export interface Classifier {
-    classify(pdfPath: string): Promise<ReportParser | undefined>;
+  classify(pdfPath: string): Promise<ReportParser | undefined>;
 }
 
 export function createClassifier(): Classifier {
-    return {
-        classify,
+  return {
+    classify,
+  };
+
+  async function classify(pdfPath: string): Promise<ReportParser | undefined> {
+    const pdfContents = await pdfToText(pdfPath);
+    if (pdfContents.indexOf("Dexcom") > -1) {
+      return dexcomParser;
+    }
+    if (pdfContents.indexOf("MiniMed 640G") > -1) {
+      return medtronik640GParser;
     }
 
-    async function classify(pdfPath: string): Promise<ReportParser | undefined> {
-        const pdfContents = await pdfToText(pdfPath);
-        if (pdfContents.indexOf('Dexcom') > -1) {
-            return dexcomParser;
-        }
-        if (pdfContents.indexOf('MiniMed 640G') > -1) {
-          return medtronik640GParser;
-        }
+    if (pdfContents.indexOf("AGP report") > -1) {
+      return libreAGPParser;
     }
+  }
 }
