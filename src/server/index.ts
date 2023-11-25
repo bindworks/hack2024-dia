cb058fac23666cfb8adaee4ae45a8cd81c9af888
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-11-26 00:37:32 +0100

    add caddy file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { unlink, writeFile } from "fs";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { createClassifier } from "../classifier";
 const app = new Hono();
 app.use("/*", cors());
-app.post("/", async (c) => {
+
+app.use("/*", serveStatic({ root: "./dist" }));
+app.post("/api/scan", async (c) => {
   const body = await c.req.parseBody();
 
   if (!body.report) return c.json({ error: "No report attached" }, 400);
